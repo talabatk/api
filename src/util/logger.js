6285b8165959902e32330db690a4a24f72466cce
omar mohamed <omer.eldanin@gmail.com>
2025-03-30 18:49:13 +0200

    edit logs

--- src/util/logger.js
+++ src/util/logger.js
@@ -1,46 +1,16 @@
 const winston = require("winston");
 
-const levels = {
-  error: 0,
-  warn: 1,
-  info: 2,
-  http: 3,
-  debug: 4,
-};
-
-const colors = {
-  error: "red",
-  warn: "yellow",
-  info: "green",
-  http: "magenta",
-  debug: "white",
-};
-
-winston.addColors(colors);
-
-const format = winston.format.combine(
-  winston.format.errors({ stack: true }),
-  winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss:ms" }),
-  winston.format.colorize({ all: true }),
-  winston.format.printf((info) => {
-    let message = `${info.timestamp} ${info.level}: ${info.message}`;
-    if (info.stack) {
-      message += `\n\n------------------------------\n\n${info.stack}`;
-    }
-    message +=
-      "\n\n------------------------------------------------------------------------------------------\n";
-    return message;
-  })
-);
-
-// Only Console Transport (No File Logging)
-const transports = [new winston.transports.Console()];
-
 const Logger = winston.createLogger({
-  level: "debug",
-  levels,
-  format,
-  transports,
+  level: "error", // Only log errors in production
+  format: winston.format.combine(
+    winston.format.errors({ stack: true }),
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss:ms" }),
+    winston.format.colorize({ all: true }),
+    winston.format.printf((info) => {
+      return `${info.timestamp} ${info.level}: ${info.message}`;
+    })
+  ),
+  transports: [],
 });
 
 module.exports = Logger;
