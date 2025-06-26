70f7f3cdf815b8ba577aa2569e5715a39622e408
omar mohamed <omer.eldanin@gmail.com>
2025-06-27 02:44:02 +0300

    edit cart

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -88,7 +88,7 @@ exports.addToCart = async (req, res) => {
 
     // calculate shipping cost
     for (const e of cart.cart_products) {
-      if (+e.product.user.id !== +product.userId) {
+      if (+e.product.user.id !== +product.vendorId) {
         return res
           .status(400)
           .json({ message: "لا يمكنك طلب طلبيه بأكثر من مطعم في نفس الطلبيه" });
